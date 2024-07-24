143b4a4063df79cdc66bc63a91beb622fdb4ac00
b_ono <b_ono@eltex.co.jp>
2024-07-25 08:22:57 +0900

    Finish

--- file_reader.js
+++ file_reader.js
@@ -1,10 +1,36 @@
 const fs = require("fs").promises;
 
 // THEN-CATCH SOLUTION BELOW THIS LINE
-
-
-
-
-
+let firstName, lastName, age, hobbies;
+fs.readFile("./firstname.txt", "utf-8").then((data) => {
+  firstName = data;
+  return fs.readFile("./lastname.txt", "utf-8").then((data) => {
+    lastName = data;
+    return fs.readFile("./age.txt", "utf-8").then((data) => {
+      age = data;
+      return fs.readFile("./hobbies.txt", "utf-8").then((data) => {
+        hobbies = JSON.parse(data);
+        console.log(
+          `${firstName} ${lastName} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`
+        );
+      });
+    });
+  });
+});
 
 // ASYNC/AWAIT SOLUTION BELOW THIS LINE
+async function introduceJohn() {
+  try {
+    const firstName = await fs.readFile("./firstname.txt", "utf-8");
+    const lastName = await fs.readFile("./lastname.txt", "utf-8");
+    const age = await fs.readFile("./age.txt", "utf-8");
+    let hobbies = JSON.parse(await fs.readFile("./hobbies.txt", "utf-8"));
+    console.log(
+      `${firstName} ${lastName} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+introduceJohn();
